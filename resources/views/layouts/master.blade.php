093cb647fc8475bdf9887cb21b2bb8088856922d
muhamad habib <linkin_56@yahoo.com>
2016-10-03 09:21:04 +0700

    repair style button on UI elements

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -49,6 +49,95 @@
     <link rel="stylesheet" href="{{ URL::asset('assets/plugins/fullcalendar/fullcalendar.min.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('assets/plugins/fullcalendar/fullcalendar.print.css') }}" media="print">
     <![endif]-->
+    <style>
+        .example-modal .modal {
+            position: relative;
+            top: auto;
+            bottom: auto;
+            right: auto;
+            left: auto;
+            display: block;
+            z-index: 1;
+        }
+
+        .example-modal .modal {
+            background: transparent !important;
+        }
+    </style>
+    <style>
+        .color-palette {
+            height: 35px;
+            line-height: 35px;
+            text-align: center;
+        }
+
+        .color-palette-set {
+            margin-bottom: 15px;
+        }
+
+        .color-palette span {
+            display: none;
+            font-size: 12px;
+        }
+
+        .color-palette:hover span {
+            display: block;
+        }
+
+        .color-palette-box h4 {
+            position: absolute;
+            top: 100%;
+            left: 25px;
+            margin-top: -40px;
+            color: rgba(255, 255, 255, 0.8);
+            font-size: 12px;
+            display: block;
+            z-index: 7;
+        }
+    </style>
+    <style>
+        .bs-glyphicons {
+            padding-left: 0;
+            padding-bottom: 1px;
+            margin-bottom: 20px;
+            list-style: none;
+            overflow: hidden;
+        }
+
+        .bs-glyphicons li {
+            float: left;
+            width: 25%;
+            height: 115px;
+            padding: 10px;
+            margin: 0 -1px -1px 0;
+            font-size: 12px;
+            line-height: 1.4;
+            text-align: center;
+            border: 1px solid #ddd;
+        }
+
+        .bs-glyphicons .glyphicon {
+            margin-top: 5px;
+            margin-bottom: 10px;
+            font-size: 24px;
+        }
+
+        .bs-glyphicons .glyphicon-class {
+            display: block;
+            text-align: center;
+            word-wrap: break-word; /* Help out IE10+ with class names */
+        }
+
+        .bs-glyphicons li:hover {
+            background-color: rgba(86, 61, 124, .1);
+        }
+
+        @media (min-width: 768px) {
+            .bs-glyphicons li {
+                width: 12.5%;
+            }
+        }
+    </style>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
@@ -318,6 +407,7 @@
 <!-- fullCalendar 2.2.5 -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
 <script src="{{ URL::asset('assets/plugins/fullcalendar/fullcalendar.min.js') }}"></script>
+
 <!-- Page specific script -->
 @yield('scripts')
 </body>
